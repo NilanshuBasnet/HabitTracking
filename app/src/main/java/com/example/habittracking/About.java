645476ffcf029680d88c617e3a82c4598e758474
NilanshuBasnet <mail.nilanshu@gmail.com>
2020-12-12 14:16:22 +0545

    FYP-V1

---
+++ app/src/main/java/com/example/habittracking/About.java
@@ -0,0 +1,14 @@
+package com.example.habittracking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class About extends Dashboard {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_menu);
+    }
+}
--- app/src/main/java/com/example/habittracking/Dashboard.java
+++ app/src/main/java/com/example/habittracking/Dashboard.java
@@ -1,22 +1,37 @@
 package com.example.habittracking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.Menu;
 
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
-public class Dashboard extends AppCompatActivity {
+import org.jetbrains.annotations.NotNull;
+
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+public DrawerLayout drawerLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
 
         // Making menu icon work
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
@@ -31,7 +46,9 @@ public class Dashboard extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.navigationView);
         navigationView.setItemIconTintList(null);
 
+
     }
+
 
     @Override
     protected void onStart() {
@@ -57,4 +74,26 @@ public class Dashboard extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.menuAbout) {
+            // Handle the camera action
+            Intent intent =new Intent(this, About.class);
+            startActivity(intent);
+        } else if (id == R.id.menuHabits) {
+
+        } else if (id == R.id.menuToday) {
+
+        } else if (id == R.id.menuTutorials) {
+
+        }
+        //close navigation drawer
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
